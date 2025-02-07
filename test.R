854d078368f4733a99b367c838a567c363e54172
LoatileLebala <lblloa001@myuct.ac.za>
2025-02-07 21:33:40 +0200

    Add Demonstration file

---
+++ test.R
@@ -0,0 +1,15 @@
+if (!requireNamespace("usethis", quietly = TRUE)) {
+  install.packages("usethis")
+}
+gert::git_config_global()
+
+# set your username and your email address
+username <- "LoatileLebala"  # replace NULL with e.g. "Miguel Rodo" (be sure to include the quotes)
+email <- "lblloa001@myuct.ac.za" # replace NULL with e.g. "miguel.rodo@uct.ac.za"
+stopifnot(!is.null(username)) # just checking you set this
+stopifnot(!is.null(email))
+gert::git_config_global_set("user.name",  username)
+gert::git_config_global_set("user.email", email)
+usethis::use_git()
+usethis::create_github_token()
+gitcreds::gitcreds_set()
